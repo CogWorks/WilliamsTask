a1d60a619eeae7a3c4731e990d1efb83989270c5
Ryan Hope <rmh3093@gmail.com>
2010-07-24 12:47:09 -0400

    get cell origins

--- williams67.c
+++ williams67.c
@@ -11,6 +11,37 @@ typedef struct {
 	unsigned long   status;
 } Hints;
 
+XPoint ** get_cell_origins(xres, yres, n) {
+	XPoint **points = malloc(n*sizeof(XPoint*));
+	int min_res = -1;
+	int res_diff = -1;
+	int i, j;
+	if (yres<xres) {
+		min_res = yres / n;
+		res_diff = ( xres - yres ) / 2;
+		for (i=0;i<n;i++) {
+			points[i] = malloc(n*sizeof(XPoint*));
+			for (j=0;j<n;j++) {
+				points[i][j].y = i * min_res + (min_res / 2);
+				points[i][j].x = j * min_res + res_diff + (min_res / 2);
+				//printf("x: %d, y: %d\n", points[i][j].x, points[i][j].y);
+			}
+		}
+	} else {
+		min_res = xres / n;
+		res_diff = ( yres - xres ) / 2;
+		for (i=0;i<n;i++) {
+			points[i] = malloc(n*sizeof(XPoint*));
+			for (j=0;j<n;j++) {
+				points[i][j].y = i * min_res + res_diff + (min_res / 2);
+				points[i][j].x = j * min_res + (min_res / 2);
+				//printf("x: %d, y: %d\n", points[i][j].x, points[i][j].y);
+			}
+		}
+	}
+	return points;
+}
+
 int main(void) {
 	Display *d;
 	Window w;
@@ -56,26 +87,53 @@ int main(void) {
 	/* map (show) the window */
 	XMapWindow(d, w);
 
+	XPoint **points = get_cell_origins(XDisplayWidth(d, s), XDisplayHeight(d, s), 13);
+	int i, j;
+
 	/* event loop */
 	while (1) {
 		XNextEvent(d, &e);
 		/* draw or redraw the window */
 		if (e.type == Expose) {
+			for (i=0;i<13;i++) {
+				for (j=0;j<13;j++) {
+					XDrawPoint(d, w, DefaultGC(d, s), points[i][j].x, points[i][j].y);
+				}
+			}
 			XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
 			XFillRectangle(d, w, DefaultGC(d, s), 550, 50, 10, 10);
 			XFillRectangle(d, w, DefaultGC(d, s), 1000, 400, 10, 10);
 			XFillRectangle(d, w, DefaultGC(d, s), 40, 500, 10, 10);
-			XPoint points[3];
-			points[0].x = 13;
-			points[0].y = 53;
-			points[1].x = 133;
-			points[1].y = 63;
-			points[2].x = 83;
-			points[2].y = 83;
-			points[3].x = 13;
-			points[3].y = 53;
-			XFillPolygon(d, w, DefaultGC(d, s), points, 4, Nonconvex, CoordModeOrigin);
+			XPoint points[13];
+			points[0].x = 100;
+			points[0].y = 100;
+			points[1].x = 110;
+			points[1].y = 100;
+			points[2].x = 110;
+			points[2].y = 110;
+			points[3].x = 120;
+			points[3].y = 110;
+			points[4].x = 120;
+			points[4].y = 100;
+			points[5].x = 130;
+			points[5].y = 100;
+			points[6].x = 130;
+			points[6].y = 90;
+			points[7].x = 120;
+			points[7].y = 90;
+			points[8].x = 120;
+			points[8].y = 80;
+			points[9].x = 110;
+			points[9].y = 80;
+			points[10].x = 110;
+			points[10].y = 90;
+			points[11].x = 100;
+			points[11].y = 90;
+			points[12].x = 100;
+			points[12].y = 90;
+			XFillPolygon(d, w, DefaultGC(d, s), points, 13, Nonconvex, CoordModeOrigin);
 			XDrawString(d, w, DefaultGC(d, s), 50, 50, msg, strlen(msg));
+			XFillArc(d, w, DefaultGC(d, s), 500, 300, 40, 40, 0, 23040);
 		}
 		/* exit on key press */
 		if (e.type == KeyPress)
