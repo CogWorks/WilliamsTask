4228f2024dc54405a2c494ac0efb0981ceaddf4c
Ryan Hope <rmh3093@gmail.com>
2010-11-22 09:08:51 -0500

    port to python

---
+++ w67.py
@@ -0,0 +1,245 @@
+#/usr/bin/env python
+"""
+This is a replicate of the classic Williams '67 visual search task.
+"""
+import sys
+import random
+import math
+import datetime
+import pygame
+
+pygame.init()
+
+shapes = {
+          "circle":"E",
+          "square":"K",
+          "oval":"F",
+          "diamond":"T",
+          "crescent":"Q",
+          "cross":"R",
+          "star":"C",
+          "triangle":"A"
+          }
+
+colors = {
+          "pink": (255,192,203),
+          "blue": (0,0,255),
+          "yellow": (255,255,0),
+          "orange": (255,165,0),
+          "green": (0,255,0)
+          }
+
+
+exp_shapes = ["star","cross","crescent","diamond","oval"]
+exp_colors = ["pink","blue","yellow","green","orange"]
+exp_sizes = ["large","medium","small","tiny"]
+
+class Shape(object):
+    """Shape object"""
+    
+    def __init__(self, world, size, name, color, id, location):
+        super(Shape, self).__init__()
+        self.shape = name
+        self.color = color
+        self.size = size
+        self.id = id
+        self.surface = world.fonts[size].render(shapes[name],True,colors[color])
+        self.surface = pygame.transform.rotate(self.surface, random.randint(1,360))
+        self.rect = self.surface.get_rect()
+        self.rect.centerx = location[0]
+        self.rect.centery = location[1]
+        self.bounding_rect = self.surface.get_bounding_rect()
+        self.id_t = world.id_font.render(str(id), True, (0,0,0))
+        self.id_rect = self.id_t.get_rect()
+        self.id_rect.centerx = location[0] + random.uniform(-world.jitter/2,world.jitter/2)
+        self.id_rect.centery = location[1] + random.uniform(-world.jitter/2,world.jitter/2)
+
+    def clickCheck(self, position):
+        return self.rect.collidepoint(position)
+        
+    def printInfo(self):
+        print self.id,self.size,self.color,self.shape
+
+
+class Probe(object):
+    """Probe objects"""
+    
+    def __init__(self, world, shape):
+        super(Probe, self).__init__()
+        
+        self.id = shape.id
+        self.shape = shape.shape
+        self.size = shape.size
+        self.color = shape.color
+        self.elements = list()
+        
+        self.id_t = world.probe_font.render(str(self.id), True, (0,0,0))
+        self.id_rect = self.id_t.get_rect()
+        self.shape_t = world.probe_font.render(self.shape, True, (0,0,0))
+        self.shape_rect = self.shape_t.get_rect()
+        self.size_t = world.probe_font.render(self.size, True, (0,0,0))
+        self.size_rect = self.size_t.get_rect()
+        self.color_t = world.probe_font.render(self.color, True, (0,0,0))
+        self.color_rect = self.color_t.get_rect()
+        
+        self.show_shape = random.randint(0,1)
+        self.show_size = random.randint(0,1)
+        self.show_color = random.randint(0,1)
+        
+        if self.show_shape:
+            self.elements.append((self.shape_t,self.shape_rect))
+        if self.show_size:
+            self.elements.append((self.size_t,self.size_rect))
+        if self.show_color:
+            self.elements.append((self.color_t,self.color_rect))
+        random.shuffle(self.elements)
+        
+        self.id_rect.centerx = world.worldsurf_rect.centerx - world.xoffset
+        self.shape_rect.centerx = world.worldsurf_rect.centerx - world.xoffset
+        self.size_rect.centerx = world.worldsurf_rect.centerx - world.xoffset
+        self.color_rect.centerx = world.worldsurf_rect.centerx - world.xoffset
+        
+        elmlen = len(self.elements)
+        if elmlen ==0:
+            self.id_rect.centery = world.worldsurf_rect.centery
+        elif elmlen == 1:
+            self.id_rect.centery = world.worldsurf_rect.centery - world.cell_side/6*.6
+            self.elements[0][1].centery = world.worldsurf_rect.centery + world.cell_side/6*.6
+        elif elmlen == 2:
+            self.id_rect.centery = world.worldsurf_rect.centery - world.cell_side/6*1.3
+            self.elements[0][1].centery = world.worldsurf_rect.centery
+            self.elements[1][1].centery = world.worldsurf_rect.centery + world.cell_side/6*1.3
+        elif elmlen == 3:
+            self.id_rect.centery = world.worldsurf_rect.centery - world.cell_side/6*1.9
+            self.elements[0][1].centery = world.worldsurf_rect.centery - world.cell_side/6*.6
+            self.elements[1][1].centery = world.worldsurf_rect.centery + world.cell_side/6*.6
+            self.elements[2][1].centery = world.worldsurf_rect.centery + world.cell_side/6*1.9
+
+
+class World(object):
+    """Main game application"""
+
+    def __init__(self):
+        super(World, self).__init__()
+        pygame.mouse.set_visible(False)
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        current_x, current_y = self.screen.get_size()
+        self.xoffset = current_x/2-current_y/2
+        self.jitter = current_y * 0.0125
+        self.nrows = 13
+        self.ncols = 13
+        self.ncolors = 5
+        self.nshapes = 5
+        self.nsizes = 4
+        self.nobjects = self.ncolors * self.nshapes * self.nsizes
+        self.ncells = self.nrows * self.ncols
+        self.query_cell = math.ceil(self.ncols*self.nrows/2)
+        self.cell_side = current_y / self.nrows
+        self.worldsurf = pygame.Surface((current_y, current_y))
+        self.worldsurf.fill((127,127,127))
+        self.worldsurf_rect = self.worldsurf.get_rect()
+        self.worldsurf_rect.centerx = current_x / 2
+        self.fonts = {
+                      "large": pygame.font.Font("cutouts.ttf", self.cell_side),
+                      "medium": pygame.font.Font("cutouts.ttf", int(self.cell_side*(1-.5/3*1))),
+                      "small": pygame.font.Font("cutouts.ttf", int(self.cell_side*(1-.5/3*2))),
+                      "tiny": pygame.font.Font("cutouts.ttf", int(self.cell_side*(1-.5/3*3)))
+                      }
+        self.cells = list()
+        self.objects = list()
+        used = list()
+        ids = list()
+        self.id_font = pygame.font.Font("freesans.ttf", self.cell_side/7)
+        
+        while len(self.cells) < self.nobjects:
+            i = random.randint(1,self.ncells)
+            if i == self.query_cell: continue
+            if self.cells.count(i) == 0:
+                r = math.floor(i / self.ncols)
+                c = i - r * self.ncols
+                o = (exp_shapes[random.randint(0,4)],exp_colors[random.randint(0,4)], \
+                     exp_sizes[random.randint(0,3)])
+                if used.count(o) == 0:
+                    id = 0
+                    while True:
+                        id = random.randint(0,99)
+                        if ids.count(id) == 0: break
+                    ids.append(id)
+                    x = (c+.5)*self.cell_side + random.uniform(-self.jitter,self.jitter)
+                    y = (r+.5)*self.cell_side + random.uniform(-self.jitter,self.jitter)
+                    s = Shape(self, o[2], o[0], o[1], id, (x,y))
+                    self.cells.append(i)
+                    used.append(o)
+                    self.objects.append(s)
+                    
+        self.probe_font = pygame.font.Font("freesans.ttf", self.cell_side/6)
+        self.probe = Probe(self, self.objects[random.randint(0,99)])
+        self.worldsurf.blit(self.probe.id_t, self.probe.id_rect)
+        for pelm in self.probe.elements:
+            self.worldsurf.blit(pelm[0], pelm[1])
+        self.screen.blit(self.worldsurf, self.worldsurf_rect)
+        pygame.display.flip()
+        
+        cont = True
+        while cont:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    cont = False
+                    break
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        sys.exit()
+                    else:
+                        cont = False
+                        break
+        
+        for object in self.objects:
+            self.worldsurf.blit(object.surface, object.rect)
+            self.worldsurf.blit(object.id_t, object.id_rect)
+        self.screen.blit(self.worldsurf, self.worldsurf_rect)
+        pygame.display.flip()
+        
+        self.end_time = 0
+        pygame.mouse.set_pos(current_x/2, current_y/2)
+        pygame.mouse.set_visible(True)
+        self.start_time = datetime.datetime.now()
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.end_time = datetime.datetime.now()-self.start_time
+                        for object in self.objects:
+                            mousex, mousey = event.pos
+                            mousex = mousex - self.xoffset
+                            if object.clickCheck((mousex,mousey)):
+                                if object.id == self.probe.id:
+                                    time_t = str(self.end_time.seconds*1000+self.end_time.microseconds*0.001)
+                                    pygame.mouse.set_visible(False)
+                                    print self.probe.id,
+                                    if self.probe.show_size: print self.probe.size,
+                                    if self.probe.show_color: print self.probe.color,
+                                    if self.probe.show_shape: print self.probe.shape,
+                                    print time_t
+                                    score_font = pygame.font.Font("freesans.ttf", self.cell_side/3)
+                                    msg = "Found target in "+time_t+" miliseconds."
+                                    time = score_font.render(msg, True, (255,255,255))
+                                    time_rect = time.get_rect()
+                                    time_rect.centerx = current_x/2
+                                    time_rect.centery = current_y/2
+                                    self.screen.fill((0,0,0))
+                                    self.screen.blit(time, time_rect)
+                                    pygame.display.flip()
+                                    cont = True
+                                    while cont:
+                                        for event in pygame.event.get():
+                                            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                                                cont = False
+                                                break
+                                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        sys.exit()
+
+if __name__ == '__main__':
+    w = World()
