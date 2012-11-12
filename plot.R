7191e4e9ed7da88a7f53900c46165d7bfd23e06d
Ryan Hope <rmh3093@gmail.com>
2012-11-12 15:11:34 -0500

    update plot code to exclude black regions on sides

--- plot.R
+++ plot.R
@@ -9,15 +9,17 @@ plot_trial <- function(archive, trial) {
   img <- paste(base,sprintf("trial-%02d.png", trial),sep="/")
   untar(archive,compressed="gzip",exdir=tempdir(), extras="-m",files=c(log,img))
   d <- subset(read.delim(paste(tempdir(),log,sep="/")), state=="SEARCH")
-  rimg <- readPNG(paste(tempdir(),img,sep="/"), FALSE)
+  rimg <- as.raster(readPNG(paste(tempdir(),img,sep="/"), FALSE))
+  rimg <- rimg[,315:1365]
   p1 <- xyplot(smi_syl~smi_sxl,data=subset(d, event_type=="ET_SPL" & smi_dyl!=0 & smi_dyr!=0 & smi_dxl!=0 & smi_dxr!=0),
-               col="blue", type="b", aspect=1050/1680, ylim=c(0,1050), xlim=c(0,1680),
+               col="blue", type="b", aspect=1, ylim=c(0,1050), xlim=c(315,1365),
                panel=function(...) {
-                 grid.raster(as.raster(rimg))
+                 grid.raster(rimg)
                  panel.xyplot(...)
                })
-  p1 <- p1 + xyplot(smi_fy~smi_fx,data=subset(d, smi_type=="r"), col="red", aspect=1050/1680, ylim=c(0,1050), xlim=c(0,1680))
-  p1 <- p1 + xyplot(mouse_y~mouse_x,data=subset(d, event_id=="MOUSE_MOTION" | event_id=="MOUSE_RESET"), type="b", col="green", aspect=1050/1680, ylim=c(0,1050), xlim=c(0,1680))
+  p1 <- p1 + xyplot(smi_fy~smi_fx,data=subset(d, smi_type=="r"), col="red", aspect=1, ylim=c(0,1050), xlim=c(315,1365))
+  p1 <- p1 + xyplot(mouse_y~mouse_x,data=subset(d, event_id=="MOUSE_MOTION" | event_id=="MOUSE_RESET"), type="b", col="green", aspect=1, ylim=c(0,1050), xlim=c(315,1365))
   print(p1)
   unlink(paste(tempdir(),base,sep="/"),T,T)
+  return(rimg)
 }
