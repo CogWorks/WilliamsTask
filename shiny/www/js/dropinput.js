928dc4c974d6b02744bfbba284069bba17771f89
Ryan Hope <rmh3093@gmail.com>
2013-02-11 15:55:40 -0500

    delete more plot code

--- shiny/www/js/dropinput.js
+++
@@ -1,8 +0,0 @@
-$(document).on("drop", "div.dropinput", function(evt) {
-  var files = evt.target.files || evt.dataTransfer.files;
-  for (var i = 0, f; f = files[i]; i++) {  
-    console.log(f);
-  }
-  evt.stopPropagation();
-  evt.preventDefault();
-});
